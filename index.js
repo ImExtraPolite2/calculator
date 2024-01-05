a64cc90a5bf0f38c6574183f1834764ab786aa63
ImExtraPolite <imextrapolite@gmail.com>
2024-01-05 17:49:41 -0500

    make button click update display

--- index.js
+++ index.js
@@ -28,8 +28,50 @@ const operate = (num1, num2, operator) => {
   }
 }
 
-let firstNum = prompt('Enter first number: ');
-let secondNum = prompt('Enter second number: ');
-let operator = prompt('Enter operator: ');
+const populateDisplay = () => {
+  const allNumbers = document.querySelectorAll('.number-button');
+  const allOperators = document.querySelectorAll('.operator-button');
+  const displayFirst = document.querySelector('.first-number');
+  const displayOperator = document.querySelector('.operator');
+  const displaySecond = document.querySelector('second-number');
 
-operate(firstNum, secondNum, operator);
+  allNumbers.forEach(number => {
+    number.addEventListener('click', () => {
+      expression += number.textContent;
+      console.log(expression);
+    });
+  });
+
+  allOperators.forEach(operator => {
+    operator.addEventListener('click', () => {
+      expression += operator.textContent;
+      console.log(expression);
+    });
+  });
+
+  if (expression.includes('+', '-', '/', '*')) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+
+  clearDisplay();
+}
+
+const clearDisplay = () => {
+  const clear = document.querySelector('.clear');
+  const display = document.querySelector('.display');
+
+  clear.addEventListener('click', () => {
+    expression = '';
+  });
+}
+
+let expression = '';
+populateDisplay();
+
+// let firstNum = prompt('Enter first number: ');
+// let secondNum = prompt('Enter second number: ');
+// let operator = prompt('Enter operator: ');
+
+// operate(firstNum, secondNum, operator);
